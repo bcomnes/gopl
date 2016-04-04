81fc4ac8bf933394f14b13006238f3e2cbf97a1f
Bret Comnes <bcomnes@gmail.com>
2016-04-03 21:01:15 -0700

    finished 1.8

---
+++ ch1/ex1.8/main.go
@@ -0,0 +1,45 @@
+// “Exercise 1.8: Modify fetch to add the prefix http:// to each argument
+// URL if it is missing. You might want to use strings.HasPrefix.”
+
+// Excerpt From: Brian W. Kernighan. “The Go Programming Language
+// (Addison-Wesley Professional Computing Series).” iBooks.
+
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		url = "http://" + url
+		fmt.Println(url)
+		if prefix := strings.HasPrefix(url, "http://"); !prefix {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+//!-
