e83726f1ce4b711d3f8b15f9c38b397d9194ab23
Bret Comnes <bcomnes@gmail.com>
2016-04-03 18:09:54 -0700

    added examples

--- ch1/ex1.6/main.go
+++ ch1/ex1.6/main.go
@@ -1,1 +1,111 @@
-dskjflsadjk
+//“Exercise 1.5: Change the Lissajous program’s color palette to green on black,
+//for added authenticity. To create the web color #RRGGBB, use color.RGBA{0xRR, 0xGG, 0xBB, 0xff},
+// where each pair of hexadecimal digits represents the intensity of the red, green, or
+// blue component of the pixel.”
+//
+//Excerpt From: Brian W. Kernighan. “The Go Programming Language
+//(Addison-Wesley Professional Computing Series).” iBooks.
+
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Run with "web" command-line argument for web server.
+// See page 13.
+//!+main
+
+// Lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+//!-main
+// Packages not needed by version in book.
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+//!+main
+
+var palette = []color.Color{
+	color.RGBA{0x00, 0x04, 0x0f, 0xff},
+	color.RGBA{0x03, 0x26, 0x28, 0xff},
+	color.RGBA{0x07, 0x3e, 0x1e, 0xff},
+	color.RGBA{0x18, 0x55, 0x08, 0xff},
+	color.RGBA{0x5f, 0x6e, 0x0f, 0xff},
+	color.RGBA{0x84, 0x50, 0x19, 0xff},
+	color.RGBA{0x9b, 0x30, 0x22, 0xff},
+	color.RGBA{0xb4, 0x92, 0x2f, 0xff},
+	color.RGBA{0x94, 0xca, 0x3d, 0xff},
+	color.RGBA{0x4f, 0xd5, 0x51, 0xff},
+	color.RGBA{0x66, 0xff, 0xb3, 0xff},
+	color.RGBA{0x82, 0xc9, 0xe5, 0xff},
+	color.RGBA{0x9d, 0xa3, 0xeb, 0xff},
+	color.RGBA{0xd7, 0xb5, 0xf3, 0xff},
+	color.RGBA{0xfd, 0xd6, 0xf6, 0xff},
+	color.RGBA{0xff, 0xf0, 0xf2, 0xff},
+}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	//!-main
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		//!+http
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			lissajous(w)
+		}
+		http.HandleFunc("/", handler)
+		//!-http
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	//!+main
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				uint8(i))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
+
+//!-main
